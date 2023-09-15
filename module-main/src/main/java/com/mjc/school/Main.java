8f7dfff249b843391195d0e299d6ac6c0b6ae533
ZakharSytoi <zakharsytoi@gmail.com>
2023-09-15 22:28:30 +0200

    migration from springdoc to springfox

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -3,8 +3,6 @@ package com.mjc.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 //@ComponentScan({"com.mjc.school.repository", "com.mjc.school.service", "com.mjc.school.controller"})
 public class Main {
